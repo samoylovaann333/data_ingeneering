d08b4a230527c03c7188f83cdeb661393554ba4e
samoylovaann333 <samoylovaann1533@gmail.com>
2025-09-23 15:54:20 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,7 +1,22 @@
 import pandas as pd
+import wget
+import os
+
 FILE_ID = "1aUvCzNoEHzLiKqYh9t9MZ-8wU-qtNFNS"
 file_url = f"https://drive.google.com/uc?id={FILE_ID}"
 
-raw_data = pd.read_csv(file_url, encoding='latin1')
+# Папка для хранения данных
+data_folder = "data"
+os.makedirs(data_folder, exist_ok=True)
 
+# Путь для сохранения файла
+file_path = os.path.join(data_folder, "dataset.csv")
+
+# Скачиваем файл
+wget.download(file_url, file_path)
+
+# Читаем CSV
+raw_data = pd.read_csv(file_path, sep=' ', encoding='latin1')
 print(raw_data.head(10))
+
+
