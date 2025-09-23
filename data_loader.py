68f811de63a7115965a01b2d1bf9dc967a92f1c5
samoylovaann333 <samoylovaann1533@gmail.com>
2025-09-23 15:37:37 +0300

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -1,6 +1,7 @@
 import pandas as pd
-def load_data():
-    FILE_ID = "16T11Jo7CuSfgaadz5CLdwTyQRvrDLvSM"
-    file_url = f"https://drive.google.com/uc?id={FILE_ID}"
-    raw_data = pd.read_csv(file_url)
-    print(raw_data.head(10))
+FILE_ID = "1aUvCzNoEHzLiKqYh9t9MZ-8wU-qtNFNS"
+file_url = f"https://drive.google.com/uc?id={FILE_ID}"
+
+raw_data = pd.read_csv(file_url, encoding='latin1')
+
+print(raw_data.head(10))
